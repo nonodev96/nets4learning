261f17bb7e30868138bae8265ce99702f6666b02
Antonio Mudarra Machuca <amm00220@red.ujaen.es>
2023-01-19 14:36:10 +0100

    ModelReviewObjectDetection

---
+++ src/Constantes.js
@@ -0,0 +1,8 @@
+const CONSOLE_LOG_h1 = 'background: #222; color: #bada55; font-size: 2em'
+const CONSOLE_LOG_h2 = 'background: #222; color: #bada55; font-size: 1.5em'
+const CONSOLE_LOG_h3 = 'background: #222; color: #bada55; font-size: 1.25em'
+export {
+  CONSOLE_LOG_h1,
+  CONSOLE_LOG_h2,
+  CONSOLE_LOG_h3
+}
