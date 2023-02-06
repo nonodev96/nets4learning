96d00324200d404fb1074fd7cfd8e6ed66d1373a
afmoya <afmoya@ujaen.es>
2023-02-06 13:07:08 +0100

    Voy a hacer pruebas para asegurarme de que el fichero funciona correctamente. Pero a grandes rasgos esta es la idea.

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Creación del build
+#npm run build
+
+#Compresión del build
+zip -r build.zip build
+
+#Copia en el servidor
+echo "¿Con qué usuario vas a copiar?"
+read usuario
+
+
+
+echo $usuario
+scp build.zip $usuario@simidat.ujaen.es:/home/$usuario
+
+#Ver el contenedor que necesitamos
+ssh -t $usuario@simidat.ujaen.es "sudo docker ps"
+
+#Copiar archivo en contenedor
+echo "¿En que contenedor quieres copiar el archivo?"
+read contenedor
+ssh -t $usuario@simidat.ujaen.es "sudo docker cp build.zip" + $contenedor +"/usr/src/app"
+
+#Descomprimir archivo en contenedor
+ssh -t $usuario@simidat.ujaen.es "sudo docker exec " + $contenedor + "unzip build.zip"
+
+#Levantar servicio
+ssh -t $usuario@simidat.ujaen.es "sudo docker exec " + $contenedor + "serve ."
