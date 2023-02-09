494b1bc066061b0dcebfd655ae156b4b5ab48b06
amm00220 <amm00220@red.ujaen.es>
2023-02-09 10:06:38 +0100

    Footer

--- src/components/footer/N4LFooter.js
+++ src/components/footer/N4LFooter.js
@@ -8,6 +8,7 @@ export default function N4LFooter() {
       <footer className="footer mt-auto py-3 bg-light">
         <Container>
           <Row>
+            {/*
             <div className="col-md-4 item">
               <h3>Acerca de</h3>
               <ul>
@@ -28,13 +29,57 @@ export default function N4LFooter() {
                 </li>
               </ul>
             </div>
-            <div className="col-md-4 item text">
-              <h3>Nets4learning</h3>
+            */}
+            <div className="col-xs-12 col-sm-12 col-md-4 item text">
+              <h4>Nets4learning</h4>
               <p>
                 Esta aplicación web forma parte de un Trabajo de Fin de Grado de Ingeniería Informática de la
                 Universidad de Jaén "Plataforma Web para el diseño y ejecución de modelos de aprendizaje profundo"
               </p>
             </div>
+            <div className="col-xs-12 col-sm-12 col-md-8 item">
+              <h3>Acerca de:</h3>
+              <ul style={{padding: "0 1em"}}>
+                <li>
+                  Autor: <a target="_blank"
+                            href="https://github.com/Davavico22"
+                            className="link-secondary">David Valdivia Vico</a>
+                </li>
+                <li>
+                  Directores: Antonio Jesús Rivera Rivas, María Dolores Pérez Godoy
+                </li>
+                <li>
+                  Mantenimiento y mejoras: Alejandro Cruz Fernandez de Moya, Antonio Mudarra Machuca
+                </li>
+                <li>
+                  <a target="_blank"
+                     rel="noreferrer"
+                     className="link-secondary"
+                     href="https://simidat.ujaen.es/">
+                    Grupo de investigación del SIMIDAT
+                  </a>
+                </li>
+                {/*<li>
+                  <a target="_blank"
+                     rel="noreferrer"
+                     href="https://eps.Ujaen.es"
+                     class="link-secondary">
+                    Escuela Politécnica Superior de Jaén
+                  </a>
+                </li>
+                */}
+                <li>
+                  <a target="_blank"
+                     rel="noreferrer"
+                     href="https://ujaen.es"
+                     className="link-secondary">
+                    Universidad de Jaén
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            {/*
             <div className="col-md-4 item text">
               <h3>
                 <a target='_blank' rel="noreferrer" href="https://simidat.ujaen.es/"
@@ -47,6 +92,7 @@ export default function N4LFooter() {
                 mundo real.
               </p>
             </div>
+            */}
           </Row>
           {/*<p className="personal-link copyright">*/}
           {/*  <a className="personal-link" target="blanck" href="https://dvalvico.es">David Valdivia </a>© 2022*/}
